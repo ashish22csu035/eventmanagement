2d24dde07951d0ce1ac8ed21119f81da2233ff52
ashish22csu035 <141615734+ashish22csu035@users.noreply.github.com>
2025-11-02 01:19:09 +0530

    changes

--- configurationserver/src/main/java/com/event/configurationserver/ConfigurationserverApplication.java
+++ configurationserver/src/main/java/com/event/configurationserver/ConfigurationserverApplication.java
@@ -3,6 +3,14 @@ package com.event.configurationserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+/*
+ * http://localhost:8888/bookingservice/default
+ * http://localhost:8888/event/default
+ * http://localhost:8888/user/default
+ * http://localhost:8888/apigateway/default
+ * http://localhost:8888/servicediscovery/default
+ * 
+ */
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,7 +18,17 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigurationserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigurationserverApplication.class, args);
-	}
+
+        SpringApplication app = new SpringApplication(ConfigurationserverApplication.class);
+        
+        
+        String mode = System.getenv("CONFIG_MODE");
+        if ("native".equalsIgnoreCase(mode)) {
+            app.setAdditionalProfiles("native");
+        } else {
+            app.setAdditionalProfiles("git");
+        }
+        app.run(args);
+    }
 
 }
